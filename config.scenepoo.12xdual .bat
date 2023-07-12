bb24a68765014c17fc5f00a84ed50f30ef017f43
kelpman05 <81154773+kelpman05@users.noreply.github.com>
2023-07-12 10:48:10 +0800

    Update config.scenepoo.12xdual .bat

--- config.scenepoo.12xdual .bat
+++ config.scenepoo.12xdual .bat
@@ -1,9 +1,9 @@
 @echo off  
-start cmd /k "cd /d C:\Users\miller\Desktop\PoO\POO_scenario&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 8"
-start cmd /k "cd /d C:\Users\miller\Desktop\PoO\POO_scenario&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 9"
-start cmd /k "cd /d C:\Users\miller\Desktop\PoO\POO_scenario&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 10"
-start cmd /k "cd /d C:\Users\miller\Desktop\PoO\POO_scenario&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 11"
-start cmd /k "cd /d C:\Users\miller\Desktop\PoO\POO_scenario&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 12"
+start cmd /k "cd /d C:\Users\miller\Desktop\PoO\CodePoSoNE&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 8"
+start cmd /k "cd /d C:\Users\miller\Desktop\PoO\CodePoSoNE&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 9"
+start cmd /k "cd /d C:\Users\miller\Desktop\PoO\CodePoSoNE&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 10"
+start cmd /k "cd /d C:\Users\miller\Desktop\PoO\CodePoSoNE&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 11"
+start cmd /k "cd /d C:\Users\miller\Desktop\PoO\CodePoSoNE&&pipenv run python -m distributed_consensus run -c config.scenepoo.12xdual.yaml 12"
 
 
 
